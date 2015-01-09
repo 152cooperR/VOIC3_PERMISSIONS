aa51b88c1a5072d3c636b9a28336197968911523
Nathan <nrtheprogrammer@gmail.com>
2015-01-08 22:15:48 -0800

    Removed unused imports

--- src/net/voic3/permissions/Main.java
+++ src/net/voic3/permissions/Main.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Nathan Rhodes
+ * Created by Nathan Rhodes A.K.A. 152cooperR & NathanSDK
  */
 
 public class Main extends JavaPlugin implements Listener{
